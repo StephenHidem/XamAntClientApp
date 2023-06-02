032cba1eb3bc96a01bb69a39b1ef6fc3d202a8b4
Stephen Hidem <StephenHidem@outlook.com>
2023-06-02 13:27:54 -0500

    Remove template code.

--- XamAntClientApp/XamAntClientApp/AppShell.xaml.cs
+++ XamAntClientApp/XamAntClientApp/AppShell.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-
-namespace XamAntClientApp
+﻿namespace XamAntClientApp
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
         public AppShell()
         {
             InitializeComponent();
-        }
-
-        private async void OnMenuItemClicked(object sender, EventArgs e)
-        {
-            //await Shell.Current.GoToAsync("//LoginPage");
         }
     }
 }
