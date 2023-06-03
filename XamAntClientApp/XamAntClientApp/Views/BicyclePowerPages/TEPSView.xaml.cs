8c83b84c1787fb552cdc875acacf003d8b73a012
Stephen Hidem <StephenHidem@outlook.com>
2023-06-03 15:03:42 -0500

    Created TEPSView and use in standard bike power sensors.

---
+++ XamAntClientApp/XamAntClientApp/Views/BicyclePowerPages/TEPSView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using SmallEarthTech.AntPlus.DeviceProfiles.BicyclePower;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamAntClientApp.Views.BicyclePowerPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TEPSView : ContentView
+    {
+        public static readonly BindableProperty TorqueEffectivenesAndPedalSmoothnessProperty = BindableProperty.Create(
+            "TEPS",
+            typeof(TorqueEffectivenessAndPedalSmoothness),
+            typeof(TEPSView));
+
+        public TorqueEffectivenessAndPedalSmoothness TEPS
+        {
+            get => (TorqueEffectivenessAndPedalSmoothness)GetValue(TorqueEffectivenesAndPedalSmoothnessProperty);
+            set => SetValue(TorqueEffectivenesAndPedalSmoothnessProperty, value);
+        }
+
+        public TEPSView()
+        {
+            InitializeComponent();
+        }
+    }
+}
